d2cf5c4dd78d2363fc80f6ac84eb35235fe2ba37
Sateesh <yegireddi.sateesh@gmail.com>
2019-08-24 17:13:01 +0530

    - Add new cases

--- ValidationLogicError.swift
+++ ValidationLogicError.swift
@@ -8,21 +8,41 @@
 
 import Foundation
 
-enum Field {
+enum Field: Hashable {
     case email
     case password
+    case name
+    case mobile
+    case otp
     
     var requirements: (error: String, code: Int, regex: String) {
         switch self {
         case .email:
             return ("Please enter valid email",
                     100,
-                    "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?"
-                    + "(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+                    "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}"
+                        + "[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+            )
         case .password:
             return ("Please enter valid password",
                     101,
-                    "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*<>?~_]).{8,20}$")
+                    "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*<>?~_]).{8,20}$"
+            )
+        case .name:
+            return ("Please enter valid name",
+                    102,
+                    "^[a-z,A-Z, ]{5,70}$"
+            )
+        case .mobile:
+            return ("Please enter valid mobile number",
+                    103,
+                    "^\\d{10}$"
+            )
+        case .otp:
+            return ("Please enter valid OTP",
+                    104,
+                    "[0-9]{6}"
+            )
         }
     }
 }
@@ -35,14 +55,23 @@ extension Field {
     func validateString(_ string: String?) -> Field? {
         let predicate = NSPredicate(format:"SELF MATCHES %@", requirements.regex)
         let isValid = predicate.evaluate(with: string)
-        return isValid ? self : nil
+        return isValid ? nil : self
     }
 }
 
-let email = "sateesh.com"
+let email = "sateesh@gmail.com"
+let password = "Sateesh@123"
 
-if let fieldValidation = Field.email.validateString(email) {
-    print(fieldValidation.error.domain)
-    return
+func validate() {
+    let fields = [Field.email: email,
+                  Field.password: password]
+    
+    fields.forEach { field in
+        if let validation = field.key.validateString(field.value) {
+            print(validation.error.domain)
+            return
+        }
+    }
 }
-//TODO: Show error message to user
+
+validate()
